be6d07dc1a147a424b3eb2e37af49a278c4d6feb
lucidcoderio <147671842+lucidcoderio@users.noreply.github.com>
2023-11-30 16:57:38 -0700

    webpack-babel-starter

---
+++ README.md
@@ -0,0 +1,5 @@
+npm install
+npm run serve
+navigate to http://localhost:8080/
+add code to index.js and view output
+when ready npm run build
---
+++ src/index.js
@@ -0,0 +1,25 @@
+class Animal {
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        console.log(`${this.name} makes a noise.`);
+    }
+
+}
+  
+class Dog extends Animal {
+    constructor(name) {
+        super(name);
+    }
+
+    speak() {
+        console.log(`${this.name} barks.`);
+    }
+}
+
+const d = new Dog("Bugsy");
+
+console.log(d.speak());
